499f57da347270c0635adff215a3e2b84edaa38d
IFFyArt <iffyart@icloud.com>
2025-10-24 22:20:27 +0800

    Add initial project setup with Next.js, TypeScript, and ESLint configuration
    - Created .gitignore to exclude unnecessary files and directories.
    - Added ESLint configuration for code quality.
    - Set up Next.js configuration in next.config.ts.
    - Initialized package.json and package-lock.json with dependencies.
    - Updated README.md with project setup instructions.
    - Configured TypeScript with tsconfig.json.
    - Implemented React Query setup in src/helper/react-query/index.ts.
    - Created custom _app.tsx and _document.tsx for Next.js.
    - Added a basic index.tsx page component.

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import queryClient from '@/helper/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
+import type { AppProps } from 'next/app';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
+}
